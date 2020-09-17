8c1c01fbaa0ab844dc686ce2eb29bf4c309a1b49
Vansh <44532230+aroravansh@users.noreply.github.com>
2020-09-17 18:22:37 +0530

    Update face_trainer.py

--- face_trainer.py
+++ face_trainer.py
@@ -4,19 +4,19 @@ from PIL import Image
 import os
 # Path for face image database
 path = 'C:\\Users\\Param Arora\\Downloads\\faces'
-recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer = cv2.face.LBPHFaceRecognizer_create()  #LBPH (LOCAL BINARY PATTERNS HISTOGRAMS) Face Recognizer is  included in the OpenCV package.
 detector = cv2.CascadeClassifier("C:\\Python\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml")
 
 
 # function to get the images and label data
 def get_images_and_labels(paths):
-    image_paths = [os.path.join(paths, f) for f in os.listdir(paths)]
+    image_paths = [os.path.join(paths, f) for f in os.listdir(paths)]  # To go through each image
     face_samples = []
     ids = []
     for image_path in image_paths:
         pil_img = Image.open(image_path).convert('L')   # convert it to grayscale
         img_numpy = np.array(pil_img, 'uint8')
-        id2 = int(os.path.split(image_path)[-1].split(".")[0])
+        id2 = int(os.path.split(image_path)[-1].split(".")[0])  # Getting images of a user to be appended together 
         faces = detector.detectMultiScale(img_numpy)
         for (x, y, w, h) in faces:
             face_samples.append(img_numpy[y:y+h, x:x+w])
@@ -29,6 +29,5 @@ faces1, id1 = get_images_and_labels(path)
 recognizer.train(faces1, np.array(id1))
 # Save the model into trainer/trainer.yml
 recognizer.write('C:\\docx\\trainer\\trainer.yml')
-# recognizer.save() worked on Mac, but not on Pi
 # Print the number of faces trained and end program
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(id1))))
